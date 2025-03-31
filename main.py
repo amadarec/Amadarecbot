80e323c0e491abf603fcc8098c710664ba69c381
amadarec <bouziane.ahs@gmail.com>
2025-03-31 20:43:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -91,3 +91,6 @@ def webhook():
                 send_message(chat_id, "📭 No active alerts.")
 
     return "ok"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
